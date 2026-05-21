d6c95843cfeb663a4237e73e435d2ecebb8e2e02
ChanPark03 <parkchan0302@gmail.com>
2026-05-21 12:31:54 +0900

    frame

---
+++ python_example/basic/variable.py
@@ -0,0 +1,20 @@
+def main():
+    # 선언 할 필요없음 
+    # 초기화 가 필요하면 # int_a = int() 처럼 하면 된다.
+    int_a = 10
+    print(int_a)
+    print(type(int_a))
+    int_a = "ten"
+    print(int_a)
+    print(type(int_a))
+
+    # 변수명,함수명 a_b_c... thePythonClass -> 식별자 파이썬에서는 snake_case 를 권장한다. 
+    # convention : 관례적으로 사용하는 방식 
+    # 변수명 함수명 -> 소문자로 시작
+    # 클래스명 -> 대문자로 시작 예외사항: built-in 은 소문자로 시작
+    
+    
+if __name__ == "__main__":
+    main()
+    
+
