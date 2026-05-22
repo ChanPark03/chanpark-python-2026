b254dfd8f73dd59ac47fa1d61eb7366f5d28250c
ChanPark03 <parkchan0302@gmail.com>
2026-05-22 17:31:02 +0900

    uv

---
+++ python_example/a100_request.py
@@ -0,0 +1,7 @@
+import requests 
+
+url = "https://naver.com"
+response = requests.get(url)
+
+print("status", response.status_code)
+print("text", response.text)
---
+++ python_example/uv_test/test_package/module_a.py
@@ -0,0 +1,9 @@
+module_var_a = "a 모듈의 변수이다!"
+
+def module_a_func():
+    print("a 모듈의 함수이다!")
+    
+    
+class Module_a:
+    def __repr__(self) -> str:
+        return "a 모듈의 클래스이다!"
